3f303aed6c9c98c7f54723c84b51f7fb0a73c1f4
thomas colligan <thomas.colligan@umontana.edu>
2021-10-01 16:07:40 -0600

    Conform to output standards with awk

--- harness/blastn/run_tool.sh
+++ harness/blastn/run_tool.sh
@@ -11,4 +11,4 @@ query_name=$(echo "$2" | sed 's/.tar.gz//g')
 outfile=$query_name.out
 blastn -task blastn -db shared -query "$query_name" -outfmt 7 -num_threads 1 -out "$outfile"
 
-grep -v \# "$outfile" | awk '{print $11, $12, $9, $10, $2}'
+grep -v \# "$outfile" | awk '{print $11, $12, $9, $10, $2, $1}'
